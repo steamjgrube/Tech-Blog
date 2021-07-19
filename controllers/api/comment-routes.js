8139fb524282e5ad47847f9db775274642c65a1c
Josh Grube <steamjgrube@gmail.com>
2021-07-19 17:41:56 -0400

    Old repo corrupt. importing files

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -2,13 +2,11 @@ const router = require('express').Router();
 const { Comment } = require('../../models/');
 const withAuth = require('../../utils/auth');
 
-// URL: /api/comment
 router.post('/', withAuth, async (req, res) => {
   try {
     const newComment = await Comment.create({
       comment_text: req.body.comment_text,
       post_id: req.body.post_id,
-      // TODO: SET USERID TO SESSION LOGGEDIN USERID
       userId: req.session.userId
     });
     res.json(newComment);
